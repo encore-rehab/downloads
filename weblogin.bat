06040374718733983ebb3bd60117d88f3e7e632a
encore-rehab <114004662+encore-rehab@users.noreply.github.com>
2022-09-20 10:59:05 -0400

    Add files via upload

---
+++ weblogin.bat
@@ -0,0 +1,21 @@
+@if (@CodeSection == @Batch) @then
+
+@echo off
+
+set SendKeys=CScript //nologo //E:JScript "%~F0"
+START CHROME "https://encore-rehab-wiki.com/"
+
+timeout /t 5
+
+%SendKeys% "ibektic"
+%SendKeys% "{TAB}"
+%SendKeys% "encore"
+%SendKeys% "{ENTER}"
+
+goto :EOF
+
+@end
+// JScript section
+
+var WshShell = WScript.CreateObject("WScript.Shell");
+WshShell.SendKeys(WScript.Arguments(0));
